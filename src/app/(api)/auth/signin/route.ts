67157ce6b900131dc2431ff1dbaed0c2e5388841
mohak <c.mohak2004@gmail.com>
2025-02-11 00:18:32 +0530

    Auth added

--- src/app/(api)/auth/signin/route.ts
+++ src/app/(api)/auth/signin/route.ts
@@ -1,15 +1,49 @@
 import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/db";
+import  jwt  from "jsonwebtoken";
 
 export async function POST( req: NextRequest , res : NextResponse) {
     const data = await req.json()
     const {email, username, password} = data;
 
     // database logic here
+    try {
+        const Exstuser = await prisma.user.findUnique({
+            where : {
+                email : email,
+                username : username
+            }
+        }) 
+    
+        if (!Exstuser) {
+            alert("user doesnt exist on app") // use better popup
+            return NextResponse.redirect('/Signup') //create a signup page
+        }
+    
+       const dbPassword = Exstuser.password; 
+       const userId = Exstuser.id
 
-    //jwt logic here
+       if (dbPassword === password) {
+        const token = jwt.sign({userId}, "secret");
 
-    return NextResponse.json({
-        msg : "loggedin"
-    })
+        return NextResponse.json({
+            msg : "account created",
+            token
+        }, {
+            status: 200
+        })
+        
+       }
+    
+    
+    } catch (error) {
+        return NextResponse.json({
+            msg : "some error occured",
+            error
+        }, {
+            status : 500
+        })
+    }
+    
 
 }
--- src/app/(api)/auth/signup/route.ts
+++ src/app/(api)/auth/signup/route.ts
@@ -1,15 +1,53 @@
 import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/db";
+import  jwt  from "jsonwebtoken";
 
 export async function POST( req: NextRequest , res : NextResponse) {
     const data = await req.json()
     const {email, username, password} = data;
 
     // database logic here
-
-    //jwt logic here
-
-    return NextResponse.json({
-        msg : "account created"
-    })
+    try {
+        const Exstuser = await prisma.user.findUnique({
+            where : {
+                email : email,
+                username : username
+            }
+        }) 
+    
+        if (Exstuser) {
+            alert("user Already exists") // use better popup
+            return NextResponse.redirect('/login') //create a login page
+        }
+    
+        const User = await prisma.user.create({
+            data : {
+                email : email, 
+                username : username, 
+                password: password
+            }
+        })
+    
+       const userId = User.id;
+    
+        //jwt logic here
+       const token = jwt.sign({userId}, "secret");
+        
+    
+        return NextResponse.json({
+            msg : "account created",
+            token
+        }, {
+            status: 200
+        })
+    } catch (error) {
+        return NextResponse.json({
+            msg : "some error occured",
+            error
+        }, {
+            status : 500
+        })
+    }
+    
 
 }
