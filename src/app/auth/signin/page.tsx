089495245136c965ebde51f1be29d0aa51c91654
mohak <c.mohak2004@gmail.com>
2025-02-11 14:49:14 +0530

    auth page

---
+++ src/app/auth/signin/page.tsx
@@ -0,0 +1,7 @@
+import { Form } from "@/components/form2";
+
+export default function Signup() {
+    return <div className="bg-black h-[100vh]">
+    <Form></Form>
+    </div>
+}
---
+++ src/app/auth/signup/page.tsx
@@ -0,0 +1,7 @@
+import { Form } from "@/components/form";
+
+export default function Signup() {
+    return <div className="bg-black h-[100vh]">
+    <Form></Form>
+    </div>
+}
